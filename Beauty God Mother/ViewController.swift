7d29416a72b7121d0f16e91e911cd7ce7426c4f1
Laila <anishakatumba@anishas-macintosh.local>
2019-01-04 15:18:23 +0000

    Initial Commit

---
+++ Beauty God Mother/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Beauty God Mother
+//
+//  Created by Anisha  KATUMBA  on 04/01/2019.
+//  Copyright © 2019 BGM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
