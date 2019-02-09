38befe4aa4010dc9a5e7101bebb0b0e943aa4149
Laila <anishakatumba@anishasacintosh.home>
2019-02-09 10:32:25 +0000

    Revert "icon and parts of slide questions"
    This reverts commit c9570047152efcc053f794837923116aaada70a2.

--- Beauty God Mother/ConfirmationDetailsBP.swift
+++ Beauty God Mother/ConfirmationDetailsBP.swift
@@ -10,52 +10,21 @@ import UIKit
 
 class confirmationDetailsBPViewController: UIViewController {
     
- 
-    
-   // let bearImageView: UIImageView = {
-      //  let imageView = UIImageView(image: #imageLiteral(resourceName: "bear_first"))
-        // this enables autolayout for our imageView
-        //imageView.translatesAutoresizingMaskIntoConstraints = false
-        //return imageView
-  //  }()
-    
-    let descriptionTextView: UITextView = {
-        let textView = UITextView()
-        textView.text = "we would like to get to know you"
-        textView.font = UIFont.boldSystemFont(ofSize: 18)
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.textAlignment = .center
-        textView.isEditable = false
-        textView.isScrollEnabled = false
-        return textView
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .white
+        return cv
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // here's our entry point into our app
-       // view.addSubview(bearImageView)
-        view.addSubview(descriptionTextView)
+        view.addSubview(collectionView)
+        collectionView.frame = view.frame
         
-        setupLayout()
     }
     
-    private func setupLayout() {
-       // bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-      //  bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-     //   bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-     //   bearImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
-        
-      //  descriptionTextView.topAnchor.constraint(equalTo: //bearImageView.bottomAnchor, constant: 120).isActive = true
-            
-                                                // descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        
-                                          //       descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-       
-                                              //   descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
-        
-  //  }
-    
     
     
     
@@ -65,4 +34,3 @@ class confirmationDetailsBPViewController: UIViewController {
 
 
 
-}
