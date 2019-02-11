2c03522c1717a765d7f349c9ea479e8602ec9254
Aisha <aishaalonge@hotmail.co.uk>
2019-02-11 01:43:10 +0000

    username
    providers username visible at the top

---
+++ Beauty God Mother/ProfileNavigationController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileNavigationController.swift
+//  Beauty God Mother
+//
+//  Created by aishat alonge on 07/02/2019.
+//  Copyright © 2019 BGM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+
+    
+
+ 
+
+
