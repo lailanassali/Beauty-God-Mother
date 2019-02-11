1eff7047744638cc49d5878f82c123656ca96d01
OJESS1 <44976031+OJESS1@users.noreply.github.com>
2019-02-11 21:31:23 +0000

    Revert "Revert "username""
    This reverts commit 0e45bc4b465ad6ac4923d78f6c7d2e88a6441d44.

---
+++ Beauty God Mother/MainTabBarController.swift
@@ -0,0 +1,24 @@
+//
+//  MainTabBarController.swift
+//  Beauty God Mother
+//
+//  Created by aishat alonge on 07/02/2019.
+//  Copyright © 2019 BGM. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+       
+        }
+    }
+
+
