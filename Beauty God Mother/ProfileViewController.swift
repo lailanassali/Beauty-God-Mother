c08b72ece96a55f9211cbf61ea768368bffca6f3
OJESS1 <44976031+OJESS1@users.noreply.github.com>
2019-02-11 21:32:03 +0000

    Revert "username"
    This reverts commit 2c03522c1717a765d7f349c9ea479e8602ec9254.

--- Beauty God Mother/ProfileViewController.swift
+++ Beauty God Mother/ProfileViewController.swift
@@ -8,40 +8,13 @@
 
 
 import UIKit
-import Firebase
 import FirebaseAuth
 
 
-
 class ProfileViewController: UIViewController {
-    
-   
-    @IBOutlet weak var orgname_label: UILabel!
-    var organisation: String?
-    
-    
     override func viewDidLoad (){
         super.viewDidLoad()
-        
-        checkIfUserIsLoggedIn()
-        
        }
-    
-    func checkIfUserIsLoggedIn() {
-        if Auth.auth().currentUser?.uid == nil {
-            perform(#selector(logout_TouchUpInside), with: nil,
-                            afterDelay: 0)
-        } else {
-            let uid = Auth.auth().currentUser?.uid
-            Database.database().reference().child("providers").child(uid!).observeSingleEvent(of: .value, with: { (snapshot) in
-                
-                if let dictionary = snapshot.value as? [String: AnyObject] {
-                self.navigationItem.title = dictionary["organisation"] as? String
-                }
-                
-            }, withCancel: nil)
-        }
-    }
     
     @IBAction func logout_TouchUpInside(_ sender: Any) {
         
