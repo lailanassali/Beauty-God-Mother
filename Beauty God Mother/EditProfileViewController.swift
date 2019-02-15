6c7f9f96a3dc0a52eeda7b3bcf0619ddcc94ef64
Laila <anishakatumba@anishasacintosh.home>
2019-02-15 15:31:37 +0000

    Revert "small"
    This reverts commit 3729cff86ae1cf53a280d5ca2b455005c86b9127.

--- Beauty God Mother/EditProfileViewController.swift
+++ Beauty God Mother/EditProfileViewController.swift
@@ -8,16 +8,49 @@
 
 import UIKit
 import Firebase
-
+import SDWebImage
 
 class EditProfileViewController: UIViewController {
 
- 
+    @IBOutlet weak var profilePictureImageView: UIImageView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var bioTextField: UITextField!
+    
+   // var databaseRef: DatabaseReference!
+    var storageRef: StorageReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+     //   databaseRef = Database.database().reference()
+        storageRef = Storage.storage().reference()
+        
+      //  loadProfileData()
 
-    
     }
+    
+   /* func loadProfileData(){
+    //    if let userID = Auth.auth().currentUser?.uid{
+    //        databaseRef.child("profile").child(userID).observe(.value, with: { (snapshot) in
+                
+            let values = snapshot.value as? NSDictionary
+                
+                if let profileImageURL = values?["photo"] as? String{
+                    self.profilePictureImageView.sd_setImage(with: URL(string: profileImageURL))
+                }
+                
+       //     })
+        }
+   // } */
+    
+
+    @IBAction func doneButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func chooseProfileButtonTapped(_ sender: Any) {
+    }
+    
+    @IBOutlet var saveButtonTapped: UIView!
 }
