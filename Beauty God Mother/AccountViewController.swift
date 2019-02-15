d845a18965035ec2a11699d6faf1cadde5d89f23
Laila <anishakatumba@anishasacintosh.home>
2019-02-15 15:03:43 +0000

    Revert "merge login to profile branch"
    This reverts commit d5d59db7e2923396ce386e3d248cfed26cf38125, reversing
    changes made to da3e020e1aca665e08190d39db80a9d4ab95c9b6.

--- Beauty God Mother/AccountViewController.swift
+++ Beauty God Mother/AccountViewController.swift
@@ -7,8 +7,6 @@
 //
 
 import UIKit
-import FirebaseAuth
-
 
 class AccountViewController: UIViewController {
 
@@ -19,17 +17,14 @@ class AccountViewController: UIViewController {
     }
     
 
-    @IBAction func logoutTapped(_ sender: Any) {
-        do {
-                try Auth.auth().signOut()
-            } catch let logoutError {
-                print(logoutError)
-            }
-            
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            let signInVC = storyboard.instantiateViewController(withIdentifier: "LoginPageID")
-            self.present(signInVC, animated: true, completion: nil)
-        }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
+    */
 
-
+}
