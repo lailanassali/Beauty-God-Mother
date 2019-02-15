d36d3ca3da52ad7c19bb94d56b5cb83da6104404
Aisha <aishaalonge@hotmail.co.uk>
2019-02-15 14:37:00 +0000

    logout
    fixed profile-accont connections

--- Beauty God Mother/AccountViewController.swift
+++ Beauty God Mother/AccountViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+
 
 class AccountViewController: UIViewController {
 
@@ -17,14 +19,17 @@ class AccountViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logoutTapped(_ sender: Any) {
+        do {
+                try Auth.auth().signOut()
+            } catch let logoutError {
+                print(logoutError)
+            }
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let signInVC = storyboard.instantiateViewController(withIdentifier: "LoginPageID")
+            self.present(signInVC, animated: true, completion: nil)
+        }
     }
-    */
 
-}
+
