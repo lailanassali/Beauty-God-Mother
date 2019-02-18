ccbbefb0521cdd2f09faf5164ce621c3662cafb0
shanju <sb874@kent.ac.uk>
2019-02-18 13:08:28 +0000

    3 swipe pages

---
+++ Beauty God Mother/Swipe3ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Swipe3ViewController.swift
+//  Beauty God Mother
+//
+//  Created by Shanju Bakeerathan on 18/02/2019.
+//  Copyright © 2019 BGM. All rights reserved.
+//
+
+import UIKit
+
+class Swipe3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
