5448805d6a556107fdfdfcd850a57ae3a32c4175
shanju <sb874@kent.ac.uk>
2019-02-26 21:12:26 +0000

    connections page

---
+++ Beauty God Mother/ConnectionsSPViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ConnectionsSPViewController.swift
+//  Beauty God Mother
+//
+//  Created by Shanju Bakeerathan on 26/02/2019.
+//  Copyright © 2019 BGM. All rights reserved.
+//
+
+import UIKit
+
+class ConnectionsSPViewController: UIViewController {
+
+  
+    @IBAction func swicth1(_ sender: UISwitch)
+    {
+        
+    }
+    
+    @IBAction func switch2(_ sender: UISwitch)
+    {
+    }
+    
+    
+    @IBAction func switch3(_ sender: UISwitch)
+    {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+   
+
+}
