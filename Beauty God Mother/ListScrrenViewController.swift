904f377b33e541e64537d4983bf3a77605d07139
shanju <sb874@kent.ac.uk>
2019-03-06 12:40:02 +0000

    image list

---
+++ Beauty God Mother/ListScrrenViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ListScrrenViewController.swift
+//  Beauty God Mother
+//
+//  Created by Shanju Bakeerathan on 06/03/2019.
+//  Copyright © 2019 BGM. All rights reserved.
+//
+
+import UIKit
+
+class ListScrrenViewController: UIViewController {
+   
+    
+}
