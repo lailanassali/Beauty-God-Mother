42dd63bc4a0770264531dd98d412e54b765a113d
Laila <anishakatumba@Anishasacintosh.home>
2019-03-10 21:14:26 +0000

    fix frame of register layout for providers
    add additional information

--- Beauty God Mother/ServicesViewController.swift
+++ Beauty God Mother/ServicesViewController.swift
@@ -22,15 +22,12 @@ class ServicesViewController: UIViewController  {
         
         self.view.addSubview(button)
         
-        button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor) .isActive = true
-        button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor) .isActive = true
-        
         button.widthAnchor.constraint(equalToConstant: 180).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 60).isActive = true
         
         button.dropView.dropDownOptions = ["Hair","Nails","Grooming","Massage"]
 
-        
+    
         
     }
     
