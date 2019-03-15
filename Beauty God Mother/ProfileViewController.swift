3ab9a1e3e3214aefdeab02ff640840102d5523bd
Aisha <aishaalonge@hotmail.co.uk>
2019-03-15 21:35:12 +0000

    Profile layout
    CHANGED PROFILE LAYOUT COMMITTTED VIEW

--- Beauty God Mother/ProfileViewController.swift
+++ Beauty God Mother/ProfileViewController.swift
@@ -11,21 +11,65 @@ import UIKit
 import FirebaseAuth
 
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+   
+  var iconArray = [UIImage(named: "webicon"),UIImage(named: "instaicon"),UIImage(named: "fbicon"),UIImage(named: "mailicon"),UIImage(named: "phoneicon"),]
+    
+    @IBOutlet weak var servicesContainer: UIView!
+    @IBOutlet weak var mediaContainer: UIView!
+    @IBOutlet weak var reviewsContainer: UIView!
+    
+    
     override func viewDidLoad (){
         super.viewDidLoad()
-       }
+    }
     
-    @IBAction func logout_TouchUpInside(_ sender: Any) {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+       return iconArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ContactCollectionViewCell", for: indexPath)
+        as? ContactCollectionViewCell
         
-        do {
-       try Auth.auth().signOut()
-        } catch let logoutError {
-            print(logoutError)
+       cell?.ContactIcons.image = iconArray[indexPath.row]
+        return cell!
+    }
+    
+    
+    @IBAction func showComponents(_ sender: AnyObject) {
+        
+        if(sender.selectedSegmentIndex == 0)
+        {
+            UIView.animate(withDuration: 0.5, animations: {
+                
+                self.servicesContainer.alpha = 1
+                self.mediaContainer.alpha = 0
+                self.reviewsContainer.alpha = 0
+            })
+                
         }
-
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-       let signInVC = storyboard.instantiateViewController(withIdentifier: "LoginPageID")
-        self.present(signInVC, animated: true, completion: nil)
+            else if (sender.selectedSegmentIndex == 1)
+        {
+            UIView.animate(withDuration: 0.5, animations: {
+                
+                self.mediaContainer.alpha = 1
+                self.servicesContainer.alpha = 0
+                self.reviewsContainer.alpha = 0
+            })
+        }
+        else
+        {
+            UIView.animate(withDuration: 0.5, animations: {
+                
+                self.reviewsContainer.alpha = 1
+                self.servicesContainer.alpha = 0
+                self.mediaContainer.alpha = 0
+            })
+        }
     }
 }
+
+
