5dc273da93d52ee9bc02fd7b66226b93ee882328
shanju <sb874@kent.ac.uk>
2019-03-21 14:51:47 +0000

    booking ui

---
+++ Beauty God Mother/BookingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BookingViewController.swift
+//  
+//
+//  Created by Shanju Bakeerathan on 21/03/2019.
+//
+
+import UIKit
+
+class BookingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
