61b913319ca06c5b8e064979e342bde13cfc9d41
shanju <sb874@kent.ac.uk>
2019-03-21 16:45:35 +0000

    calendar

--- Beauty God Mother/BookingViewController.swift
+++ Beauty God Mother/BookingViewController.swift
@@ -7,15 +7,62 @@
 
 import UIKit
 
-class BookingViewController: UIViewController {
+class BookingViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+    
 
+    
+    @IBOutlet var Calendar: UICollectionView!
+    @IBOutlet var MonthLabel: UILabel!
+    
+    let Months =
+        ["January","Febuary","March","April","May","June","July","August","September","October","November","December"]
+    
+    let DaysOfMonth = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+    
+    var DaysInMonths = [31,28,31,30,31,30,31,31,30,31,30,31]
+    
+    var currentMonth = String()
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        currentMonth = Months[month]
+        
+        MonthLabel.text = "\(currentMonth)\(year)"
+        
+    }
+    
+    
+    
+    
+    
+    
+    @IBAction func Next(_ sender: Any) {
+    }
+    
+    @IBAction func Back(_ sender: Any) {
     }
     
 
+   
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return DaysInMonths[month]
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Calendar", for: indexPath) as! DateCollectionViewCell
+        cell.backgroundColor = UIColor.clear
+        cell.DateLabel.text = "\(indexPath.row + 1)"
+        
+        return cell
+        
+    }
     
 
 }
---
+++ Beauty God Mother/CalendarVars.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarVars.swift
+//  Beauty God Mother
+//
+//  Created by Shanju Bakeerathan on 21/03/2019.
+//  Copyright © 2019 BGM. All rights reserved.
+//
+
+import Foundation
+
+
+let date = Date()
+let calendar = Calendar.current
+
+let day = calendar.component( .day, from: date)
+let weekday = calendar.component(.weekday, from: date)
+let month = calendar.component(.month, from: date)  - 1
+let year = calendar.component(.year, from: date)
