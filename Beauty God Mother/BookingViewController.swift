42086fbf753e4be8463a772be08d51b75588f18f
shanju <sb874@kent.ac.uk>
2019-03-21 17:10:08 +0000

    calendar

--- Beauty God Mother/BookingViewController.swift
+++ Beauty God Mother/BookingViewController.swift
@@ -41,15 +41,50 @@ class BookingViewController: UIViewController, UICollectionViewDelegate, UIColle
     
     
     @IBAction func Next(_ sender: Any) {
+        switch currentMonth {
+        case "December":
+            month = 0
+            year += 1
+            
+            currentMonth = Months[month]
+
+            
+            MonthLabel.text = "\(currentMonth)\(year)"
+            Calendar.reloadData()
+        default:
+            month += 1
+            
+            currentMonth = Months[month]
+
+            MonthLabel.text = "\(currentMonth)\(year)"
+            Calendar.reloadData()
     }
     
+}
+    
     @IBAction func Back(_ sender: Any) {
+        switch currentMonth {
+        case "January":
+            month = 11
+            year -= 1
+            
+            currentMonth = Months[month]
+            
+            MonthLabel.text = "\(currentMonth)\(year)"
+            Calendar.reloadData()
+            
+        default:
+            month -= 1
+            currentMonth = Months[month]
+            MonthLabel.text = "\(currentMonth)\(year)"
+            Calendar.reloadData()
+        }
+        
     }
+    
     
 
    
-    
-    
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return DaysInMonths[month]
