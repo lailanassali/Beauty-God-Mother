c833f16bca92be30c2114f3948091c87ffa83cfd
Laila <anishakatumba@anishasacintosh.home>
2019-03-27 16:56:14 +0000

    Revert "Revert "revert""
    This reverts commit 02ad82e01f8cf08af28d2437cc5da4f279be7c91.

---
+++ Beauty God MotherTests/Beauty_God_MotherTests.swift
@@ -0,0 +1,26 @@
+//
+//  Beauty_God_MotherTests.swift
+//  Beauty God MotherTests
+//
+//  Created by Anisha  KATUMBA  on 04/01/2019.
+//  Copyright © 2019 BGM. All rights reserved.
+//
+
+import XCTest
+@testable import Beauty_God_Mother
+
+class Beauty_God_MotherTests: XCTestCase {
+    
+    let sut = RBViewController()
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+
+    
+}
