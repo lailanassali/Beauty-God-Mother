4f711fde012a08805827489b426b913e01103288
Laila <anishakatumba@anishasacintosh.home>
2019-03-27 17:00:24 +0000

    Revert "Merge all code from 'develop' to 'master'"
    This reverts commit 56ae238cf9aad0d5739510994554a5114831d5b2, reversing
    changes made to 261436d692dfc0a65e4757d4280e01661494f405.

--- Pods/Target Support Files/Pods-Beauty God Mother/Pods-Beauty God Mother-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Beauty_God_Mother : NSObject
-@end
-@implementation PodsDummy_Pods_Beauty_God_Mother
-@end
--- Pods/Target Support Files/Pods-Beauty God MotherTests/Pods-Beauty God MotherTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Beauty_God_MotherTests : NSObject
-@end
-@implementation PodsDummy_Pods_Beauty_God_MotherTests
-@end
