361ec07f0dba61ab313dcf1115c7b043924dc643
OJESS1 <44976031+OJESS1@users.noreply.github.com>
2019-04-16 22:10:58 +0100

    search fixed
    Co-Authored-By: Laila <lailanassali@users.noreply.github.com>

--- Search/SearchViewController.swift
+++ Search/SearchViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class SearchViewController: UIViewController {
 
@@ -26,6 +27,12 @@ class SearchViewController: UIViewController {
     
     private var profiles: [Profile] = []
     
+    let searchController = UISearchController(searchResultsController: nil)
+    let dataProvider = NSData(contentsOfFile: Bundle.main.path(forResource: "profile", ofType: "json")!)
+    var profile: JSON?
+    var filteredprofile = [JSON]()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,18 +42,54 @@ class SearchViewController: UIViewController {
         profiles = DataProvider.shared.allProfiles
    // print(profiles[0].services)
         // Do any additional setup after loading the view.
+        
+        do {
+            profile = try JSON(data: dataProvider! as Data)
+        } catch {
+            
+        }
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
+    }
+    
+    func filterContentForSearch(searchText: String){
+        filteredprofile = profile!.array!.filter{ profile in
+            return profile["category"].stringValue.lowercased().contains(searchText.lowercased())
+            
+    }
+            
+    tableView.reloadData()
+}
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearch(searchText: searchController.searchBar.text!)
     }
 }
 
 extension SearchViewController: UITableViewDataSource {
    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searchController.isActive && searchController.searchBar.text != ""{
+         return filteredprofile.count
+        }
         return profiles.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifiers.SearchCell, for: indexPath) as! SearchCell
-        cell.configure(with: profiles[indexPath.row])
+        var data: JSON
+        if searchController.isActive && searchController.searchBar.text != ""{
+        data = filteredprofile[indexPath.row]
+        }else{
+            
+            data = profile![indexPath.row]
+        }
+          let profileCategory = data["category"].stringValue
+        cell.textLabel?.text = profileCategory
         return cell
     }
     
@@ -60,6 +103,7 @@ extension SearchViewController: UITableViewDelegate {
         guard let detailViewController = storyboard.instantiateViewController(withIdentifier: ViewControllerIdentifiers.Detail) as? DetailsViewController else {
             return
         }
+        
         detailViewController.profile = profiles[indexPath.row]
         navigationController?.pushViewController(detailViewController, animated: true)
     } 
