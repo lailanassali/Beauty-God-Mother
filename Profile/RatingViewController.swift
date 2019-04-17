9445aa30a7dec37654826ca205aa864ec326a65f
TiffanyTobiL <tbt3@kent.ac.uk>
2019-04-17 03:59:16 +0100

    rating

--- Profile/RatingViewController.swift
+++ Profile/RatingViewController.swift
@@ -2,40 +2,37 @@
 //  RatingViewController.swift
 //  Beauty God Mother
 //
-//  Created by Tiffany Tobi-Lawal on 15/04/2019.
+//  Created by Tiffany Tobi-Lawal on 17/04/2019.
 //  Copyright © 2019 BGM. All rights reserved.
 //
 
 import UIKit
 
-class RatingViewController: UIStackView {
-    var starRating = 0
-    var starsEmptyPicName = "star"
-    var starsFilledPicName = "starfill"
-    override func draw(_ rect: CGRect) {
-        let myViews = self.subviews.filter{$0 is UIButton}
-        var starTag = 1
-        for theView in myViews {
-            if let theButton = theView as? UIButton{
-                theButton.setImage(UIImage(named: starsEmptyPicName), for: .normal)
-                theButton.addTarget(self, action: #selector(self.pressed(sender:)), for: .touchUpInside)
-                theButton.tag = starTag
-                starTag = starTag + 1
-            }
+class RatingViewController: UIViewController {
+    
+    @IBOutlet var starButtons: [UIButton]!
+    
+    
+    @IBAction func starButtonTap(_ sender: UIButton) {
+    let tag = sender.tag
+        for button in starButtons {
+        if button.tag <= tag {
+            //if its less than or equal to the tag number of the tapped
+            //button then fill the button and select this button
+            button.setTitle("★", for: .normal)
+        } else {
+             button.setTitle("☆", for: .normal)
+            // show them as not selected
+        
         }
-    }
+        }
+        
 
-    @objc func pressed(sender: UIButton) {
-        starRating = sender.tag
-        let myViews = self.subviews.filter{$0 is UIButton}
-        for theView in myViews {
-            if let theButton = theView as? UIButton{
-                if theButton.tag > sender.tag {
-                    theButton.setImage(UIImage(named: starsEmptyPicName), for: .normal)
-                }else{
-                    theButton.setImage(UIImage(named: starsFilledPicName), for: .normal)
-                }
-            }
-        }
-    }
+
 }
+}
+    
+    
+    
+    
+
